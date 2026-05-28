bdb3700a2fcf1193b5d3329e1978e660d39615bb
Naveen <kolanaveen797@gmail.com>
2026-05-28 17:52:41 +0530

    ad

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Message from "./Message.jsx";
+import Databases from "./databases.jsx";
+
+function App() {
+  return (
+    <div>
+      <Databases />
+    </div>
+  );
+}
+
+export default App;
